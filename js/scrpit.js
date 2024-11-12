6a5b894f3d02fc86b9c9acfa4382d54937ec9264
NicolasJeanz <nicolasjeansantos0@gmail.com>
2024-11-11 21:56:23 -0300

    Gerenciamentos

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,5 +1,3 @@
-let spanRemovido = null;
-
 class Pedido {
         constructor(id, produto, quantidade, status ='Pendente') {
         this.id = id;
@@ -18,11 +16,42 @@ class Restaurante {
     adicionarPedido(produto, quantidade) {
         const pedido = new Pedido(this.idAtual++, produto, quantidade);
         this.pedidos.push(pedido);
+        this.atualizarStorage();
         this.montarElementoProduto();
     }
+
+    atualizarPedido(id,status) {
+        const pedido = this.pedidos.find(pedido => pedido.id === id);
+        if (pedido) {
+            pedido.status = status;
+            this.atualizarStorage();
+            this.montarElementoProduto();
+        }
+    }
+
+    removerpedido(id) {
+        const index = this.pedidos.findIndex(pedido => pedido.id === id);
+        if(index != -1) {
+            this.pedidos.splice(index, 1);
+            this.atualizarStorage();
+            this.montarElementoProduto()
+        }
+    }
+
+    atualizarStorage() {
+        localStorage.setItem("@pedidos", JSON.stringify(this.pedidos))
+    }
+    buscarStorage() {
+        const storagePedidos = localStorage.getItem("@pedidos");
+        if(storagePedidos) {
+            this.pedidos = JSON.parse(storagePedidos);
+            this.montarElementoProduto();
+        }
+    }
+
     montarElementoProduto() {
         const listaPedidos = document.getElementById("lista-pedidos");
-        listaPedidos.innerHTML= "";
+        listaPedidos.innerHTML = "";
 
         this.pedidos.forEach((pedido) => {
             const li = document.createElement("li");
@@ -32,9 +61,9 @@ class Restaurante {
             <p><strong>Quantidade:</strong> ${pedido.quantidade}</p>
             <p><strong>Status:</strong> ${pedido.status}</p>
             <div class="acoes-pedidos">
-                <button class="btn-atualizar">Em preparo</button>
-                <button class="btn-atualizar">Finalizado</button>
-                <button class="btn-remover">Remover</button>
+                <button class="btn-atualizar" onclick="atualizarStatus(${pedido.id},'em preparo')">Em preparo</button>
+                <button class="btn-atualizar" onclick="atualizarStatus(${pedido.id},'finalizado')">Finalizado</button>
+                <button class="btn-remover" onclick="removerpedido(${pedido.id})">Remover</button>
             </div>
             `;
             listaPedidos.appendChild(li);
@@ -43,6 +72,13 @@ class Restaurante {
 }
 
 const restaurante = new Restaurante();
+
+function iniciarDados(){
+    restaurante.buscarStorage()
+
+    const lista = document.getElementById("lista-pedidos");
+    this.montarElementonaoExiste(lista);
+}
 
 function adicionarpedido() {
     const produto = document.getElementById("produto").value;
@@ -55,26 +91,40 @@ function adicionarpedido() {
 
         atualizarMensagem()
     } else {
-            alert('Por favor,preencha todos os campos.')
+            alert('Por favor,preencha todos os campos.');
     
     }
 }
+
+function atualizarStatus(id,status) {
+    restaurante.atualizarPedido(id,status);
+}
+function removerpedido(id) {
+    restaurante.removerpedido(id);
+    this.atualizarMensagem();
+}
 function atualizarMensagem() {
     const lista = document.getElementById("lista-pedidos");
+    const mensagemNaoExiste = document.getElementById("nao-existe");
 
     if(lista.children.length === 0) {
-        if (spanRemovido) {
-            document.querySelector(".lista-pedidos").insertBefore(
-                spanRemovido, document.querySelector(
-                    ".lista-pedidos").firstChild);
-                spanRemovido = null;
-            
-        }
+        this.montarElementonaoExiste(lista);
     } else {
-        let mensagemNaoExiste = document.getElementById("nao-existe")
         if(mensagemNaoExiste) {
-            spanRemovido = mensagemNaoExiste;
             mensagemNaoExiste.remove();
         }
     }
 }
+function montarElementonaoExiste(lista) {
+    let mensagemNaoExiste = document.getElementById("nao-existe");
+    if (!mensagemNaoExiste && lista.children.length === 0) {
+        const li = document.createElement("li");
+        const mensagem = document.createElement("span");
+        mensagem.id = "não existe";
+        mensagem.textContent = "Não existe Pedidos!"
+        li.appendChild(mensagem)
+        lista.appendChild(li)
+    }
+}
+
+this.iniciarDados()
